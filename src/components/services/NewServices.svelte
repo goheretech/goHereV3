de73537d0d290da970cb8e2069a8442765e9e673
jacklemay <clemay.lm@gmail.com>
2021-02-03 16:44:58 -0600

    opa

---
+++ src/components/services/NewServices.svelte
@@ -0,0 +1,19 @@
+<div class="service__holder">
+  <div class="service__slider">
+    <div class="slider__button">
+      <h3>Demo Text</h3>
+    </div>
+  </div>
+  <div class="service__section">
+    <div class="service__info">
+      <h2>Lorem ipsum dolor, sit amet consectetur!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis soluta
+        placeat voluptatum ipsam, ex porro sit magni dicta laboriosam
+        consequuntur praesentium, rem beatae vitae culpa nobis iusto, harum
+        sapiente labore odit? Fugiat totam ex amet et aut a odio quis
+        perspiciatis enim.
+      </p>
+    </div>
+  </div>
+</div>
