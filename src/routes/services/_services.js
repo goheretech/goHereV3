39b01b3bb6a77a53f8c037911bdc060e0046a89b
Jack <clemay@gohere.tech>
2021-02-03 13:14:48 -0500

    fixing-changes

--- src/routes/services/_services.js
+++ src/routes/services/_services.js
@@ -23,60 +23,15 @@ export const serv = [
             '<p>Building your online presence validates your business and makes it easier for your customers to find and interact with you. Whether you need an online ordering app for your restaurant or a website for your business, we know how to design and build applications that convert.</p>',
     },
     {
-        name: 'Responsive User Interface',
+        name: 'UI/UX',
         section: 'Build',
         ico: 'fas fa-laptop-code',
         slug: 'responsive-user-interface',
-        titleTag: 'UI / UX - goHere™ for Responsive User Interfaces',
+        titleTag: 'UI/UX Artists - goHere®',
         metaDesc:
-            'Mobile first UI / UX strategies and web content that is responsive to users’ screens will ensure that your content is set up to convert desired actions. AR / VR projects open availability.',
+            'Mobile first UI/UX strategies and web content that is responsive to users’ screens will ensure that your content is set up to convert desired actions.',
         desc:
-            '<p>Responsive user interface refers to building front end designs that “respond” to the size of the screen a user is viewing a website on. Mobile first design strategy ensures your mobile website converts--which is especially important as phone website views dramatically outpace desktop website views.</p>',
-    },
-    {
-        name: 'Engaging User Experience',
-        section: 'Build',
-        ico: 'fas fa-user-plus',
-        slug: 'engaging-user-experience',
-        titleTag: 'Engaging User Experiences - goHere, Augment Your Reality™',
-        metaDesc:
-            "Augment Your Reality, go here for engaging user experiences that maintain users' attention and drive them towards your call to actions. ROI focused.",
-        desc:
-            '<p>Creating engaging user experiences is all about capturing and maintaining the attention of the user by anticipating their needs--by answering their next question. A user should flow naturally through your website, pulled along by engaging scroll movements, attention grabbing designs, and well placed buttons, forms, and links--every action imperceptibly leading them towards the desired action: conversion.</p>',
-    },
-    {
-        name: 'Platinum Level Hosting',
-        section: 'Build',
-        ico: 'fas fa-hdd',
-        slug: 'platinum-level-hosting',
-        titleTag: 'Platinum Level Hosting - Google Cloud - goHere™',
-        metaDesc:
-            'Make your website up to 200% faster with platinum level hosting through the Google Cloud Platform.',
-        desc:
-            "<p>At <span class='goHere'>go<b>Here</b></span>, we exclusively use the Google Cloud Platform for hosting, making customer’s sites up to 200% faster, more stable, secure, and completely isolated. It’ll save you money on IT and makes your system almost instantly scalable as you grow.</p>",
-    },
-    {
-        name: 'Full Google Suite Integration',
-        section: 'Build',
-        ico: 'fab fa-google',
-        slug: 'google-suite',
-        titleTag: 'Full Google Suite Integration & Optimization - goHere™',
-        metaDesc:
-            "Streamline your company for sucess. Google Suite Integration can increase efficiency and productivity in organizations when set up properly. goHere, we speak Google's language.",
-        desc:
-            '<p>The Google Suite is an ever growing, extremely useful tool for businesses of all sizes. Whether it’s marketing, productivity, or communications, Google Suite integration will help you streamline your business for success. Our expertise across the entire software suite will ensure that our clients and partners are using the Suite to its fullest potential.</p>',
-    },
-    {
-        name: 'White Labelling',
-        section: 'Build',
-        ico: 'fas fa-brush',
-        slug: 'white-labelling',
-        titleTag:
-            'White Labeled Technology Solutions - goHere, Augment Your Reality™',
-        metaDesc:
-            'As your creative return on investment partner, goHere is flexible in providing customized, white labeled technology solutions for all industries.',
-        desc:
-            "<p>At <span class='goHere'>go<b>Here</b></span>, we are your creative return on investment partner, and we are flexible in providing customized solutions that can be white labeled to your brand. When you purchase our services under a white label, you can present our products and ideas to your client under your own brand, assured that our standard of excellence will represent your brand well.</p>",
+            '<p>Responsive user interface refers to building front end designs that “respond” to the size of the screen a user is viewing a website on. Mobile first design strategy ensures your mobile website converts--which is especially important as phone website views dramatically outpace desktop website views.<br><br>Creating engaging user experiences is all about capturing and maintaining the attention of the user by anticipating their needs--by answering their next question. A user should flow naturally through your website, pulled along by engaging scroll movements, attention grabbing designs, and well placed buttons, forms, and links--every action imperceptibly leading them towards the desired action: conversion.</p>',
     },
     {
         name: 'Conversion Rate Optimization',
@@ -114,39 +69,6 @@ export const serv = [
             '<p>Deciding how you want to represent yourself and doing it in a way that will help you reach and engage new audiences is one of the most impactful ways to set your business up for success. </p><p>After vision matching to create a brand identity that represents you, we work hard to understand the wants, needs, and digital habits of your target audience. We specialize in helping our clients outcompete in saturated markets.</p>',
     },
     {
-        name: 'Content Strategy',
-        section: 'Grow',
-        ico: 'fas fa-hdd',
-        slug: 'content-strategy',
-        titleTag: 'Quality, Rich Content Strategy Company - goHere™',
-        metaDesc:
-            'The right content strategy helps establish brand awareness, drives conversions, and optimizes for search engine results.',
-        desc:
-            "<p><span class='goHere'>go<b>Here</b></span> for high quality content rich in storytelling, branding, and art! The right content strategy will help you bolster your SEO strategy, generate leads, and drive conversions for your brand.</p><p>Whether you’re looking to create compelling content for your website, generate in-house articles for your business’s blog, or craft a viral campaign, our content creators will set your brand apart.</p>",
-    },
-    {
-        name: 'Digital Marketing',
-        section: 'Grow',
-        ico: 'fab fa-google',
-        slug: 'digital-marketing',
-        titleTag: 'Digital Marketing - VR / AR Solutions - goHere™',
-        metaDesc:
-            'Augment your reality with goHere. Establish brand equity and identity that is optimized for your target audience through emerging technology solutions including Augmented Reality.',
-        desc:
-            '<p>We help you reach your target audience wherever they are--which is usually glued to their mobile devices for hours on end. Marketing your business online is the best way to find new customers and grow your business. Create a brand identity that sells, ads that convert, and a google ranking that puts you in front of more eyes.</p>',
-    },
-    {
-        name: 'Search Engine Optimization',
-        section: 'Grow',
-        ico: 'fas fa-brush',
-        slug: 'search-engine-optimization',
-        titleTag: 'Miami Visual & Local SEO - goHere™',
-        metaDesc:
-            'Organic, direct traffic to your website or application is worth its weight in gold. We at goHere speak to Google for you. Search is changing towards new channels; voice, visual, and local. Stay ahead of the curve and goHere.',
-        desc:
-            '<p>Unlike the old, big yellow phone books of previous decades, business listings are not in alphabetical order anymore when people search for services and products. The reality is that Google is the leading provider for search across the globe. The difference between being first page and not could be a crucial difference for your business’ profits for the year. Organic, direct traffic to your website is worth its weight in gold; goHere speaks Google’s language. Let us increase your brand awareness.</p>',
-    },
-    {
         name: 'Public Relations',
         section: 'Optimize',
         ico: 'fas fa-sitemap',
@@ -169,28 +91,6 @@ export const serv = [
             '<p>Your brand identity is the way your company presents itself to your audience. Refining your brands personality to best appeal to potential clients is essential for a competitive presence both on and offline. Every detail of a brands behavior impacts perception, and as such must be managed as a whole to present a coherent professional identity.</p><p>We utilize up to date information based on both market constants and trends to create a brand strategy and optimize an identity that is most appealing to your target audience, with a first and foremost goal of generating leads.</p>',
     },
     {
-        name: 'Reputation Management',
-        section: 'Optimize',
-        ico: 'fas fa-user-plus',
-        slug: 'reputation-management',
-        titleTag: 'Miami PR / Reputation Management Partner - goHere™',
-        metaDesc:
-            'Control the public narrative of your company. Tell a story that speaks and works for you. goHere to Augment your reality™.',
-        desc:
-            '<p>Under the umbrella of public relations, reputation management puts you in control of the public narrative about your business--from search results for your company name and related terms, to social media representation, to media coverage. Tell a story that sells for you.</p>',
-    },
-    {
-        name: 'Go-To-Market Strategy',
-        section: 'Optimize',
-        ico: 'fas fa-hdd',
-        slug: 'go-to-market-strategy',
-        titleTag: 'Have an Idea? Go To Market Strategy with goHere',
-        metaDesc:
-            'Utilizing data driven insights and market research, our team of engineers and business professionals have all the tools you need to facilitate any idea, period. We are your ROI partner.',
-        desc:
-            '<p>Product success starts with your go to market strategy. Using data-driven insights and market research to craft compelling messaging for your target audience and meeting them where they are on the internet will expand your reach and the quality of your engagement. </p><p>Our full business development repertoire--growth strategy, advertising, SEO, resource planning, web development, and public relations--will set you up for success in your industry. goHere when you want to outcompete in a saturated market.</p>',
-    },
-    {
         name: 'System Auditing',
         section: 'Optimize',
         ico: 'fab fa-google',
@@ -202,7 +102,7 @@ export const serv = [
             '<p>Systems can either streamline or hinder your business depending on their ease of use. Identifying barriers to efficiency in the systems used by your business will ensure that they’re an indispensable extension of your workforce, not a hindrance to your staff.</p><p>Systems created with user experience at the forefront will propel your business forward and help you easily manage projects, tasks, inventory, schedules, etc. in one, easy to use place.</p>',
     },
     {
-        name: 'Enterprise Resource Planning',
+        name: 'ERP',
         section: 'Optimize',
         ico: 'fas fa-brush',
         slug: 'enterprise-resource-planning',
@@ -213,8 +113,19 @@ export const serv = [
             '<p>If systems produce order in business, an ERP system brings ultimate order; an enterprise resource planning system glues together the systems used by different departments within an organization--finances, human resources, production, distribution, client management, etc--into one system.</p><p>Compiling separate systems into one interface creates order and reduces error for streamlined collaboration and communication. ERPs are all about maximizing efficiency and creating self-sustaining systems.</p>',
     },
     {
+        name: 'Mixed Reality',
+        section: 'Build',
+        ico: 'fas fa-brush',
+        slug: 'mixed-reality',
+        titleTag: 'Mixed Reality Consultants - Hololens 2 Developers - goHere®',
+        metaDesc:
+            'Augment your reality™ with goHere; compile old legacy systems and start developing for the next technological trend: mixed reality and augmented reality ',
+        desc:
+            '<p>Mixed reality eliminates the need for screens--and as the hardware for this technology advances, we will see screens eliminated. The future applications of MR outpace that of AR because the handsfree, heads up display nature of MR removes the need to leave your physical surroundings by immersing into a screen.</p>',
+    },
+    {
         name: 'Virtual Reality',
-        section: 'Impress',
+        section: 'Build',
         ico: 'fas fa-sitemap',
         slug: 'virtual-reality',
         detailed: true,
@@ -230,37 +141,15 @@ export const serv = [
             '<p>Unlike AR, VR disconnects you from your surroundings; like AR, VR affords incredible business and marketing opportunities. The world is changing in dramatic ways; the workforce is transitioning to working remotely and events are bound by the rules of social distancing. We’ve seen digital concerts and virtual events spark massive success. The applications of virtual reality will only grow with our evolving world.</p>',
     },
     {
-        name: 'Edge Computing',
-        section: 'Impress',
-        ico: 'fas fa-hdd',
-        slug: 'edge-computing',
-        titleTag: 'Edge Computing with goHere - Augment Your Reality™',
-        metaDesc:
-            'Process data wherever possible and act swiftly. Edge computing uses a distributed computer network that enables the internet of things for business and enteprises and will faciliate many VR and AR projects.',
-        desc:
-            '<p>Process data wherever possible and act swiftly. Welcome to edge computing; a distributed computer network that enables the internet of things (IoT) for businesses and enterprises. Bringing applications closer to the edge servers and source data brings faster analytics and reports, decreased response times and much improved bandwidth.</p>',
-    },
-    {
-        name: 'Blockchain Technology',
-        section: 'Impress',
-        ico: 'fab fa-google',
-        slug: 'blockchain-technology',
-        titleTag: 'Miami Blockchain Technology Consulting - goHere™',
-        metaDesc:
-            'Blockchain is revolutionizing the payments and financial world. Organizations will need to react to take advantage of the advantages presented by blockchain tech, goHere and speak to a professional that will guide your company forward into the digital evolution.',
-        desc:
-            '<p>Blockchain is many things. A lot of people think of Bitcoin when they hear the term. Bitcoin is the most infamous public blockchain network. But, as a whole blockchain is a database or distributed ledger system developed cryptographically. Each block in the chain represents multiple transactions with records added to every ledger on the chain every time a transaction is added. A transaction does not have to be financial. Potential uses for blockchain technology includes banking, supply chain, property records, legal documents, voting, medical records, and cryptocurrencies. </p>',
-    },
-    {
         name: 'Augmented Reality',
-        section: 'Impress',
+        section: 'Build',
         ico: 'fas fa-laptop-code',
         slug: 'augmented-reality',
-        titleTag: 'Augmented Reality Firm - goHere - Augment Your Reality™',
+        titleTag: 'Augmented Reality Developers - goHere® - Augment Your Reality™',
         metaDesc:
             'AR is the ultimate immersive technology experience that seamlessly blends the digital world with the physical. Augmented reality solutions will propel companies forward throughout this decade.',
         desc:
-            '<p>AR is the ultimate immersive technology experience that seamlessly blends the digital world with the physical. Augmented reality solutions will propel companies forward throughout this decade. </p>',
+            '<p>AR is the ultimate immersive technology experience that seamlessly blends the digital world with the physical. Augmented reality solutions will propel companies forward throughout this decade.</p><p>96% of top AR stakeholders believe glasses based augmented reality will overtake screen based AR applications within the next 3 years--44% of those predict we will see this shift within the next 1-2 years.</p>',
     },
 ];
 
